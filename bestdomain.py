f1c0ab6a083051fc4d249ef6bde5a784e19afa27
leung7963 <104255180+leung7963@users.noreply.github.com>
2024-08-27 12:16:36 +0800

    更新 bestdomain.py

--- bestdomain.py
+++ bestdomain.py
@@ -34,6 +34,11 @@ def get_huawei_record_id(client):
 def delete_existing_dns_records(client, zone_id, recordset_id, subdomain, domain):
     request = BatchDeleteRecordSetWithLineRequest()
     listRecordsetIdsbody = ['{recordset_id}']
+    request.body = BatchDeleteRecordSetWithLineRequestBody(
+            recordset_ids=listRecordsetIdsbody
+        )
+    response = client.batch_delete_record_set_with_line(request)
+    print(response)
 
 def update_huawei_dns(ip_list, client, zone_id, recordset_id, subdomain, domain):
     record_name = domain if subdomain == '@' else f'{subdomain}.{domain}'
