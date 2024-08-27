636e00e14f83da10606e4fbd74749be29f61fafc
leung7963 <104255180+leung7963@users.noreply.github.com>
2024-08-27 12:04:06 +0800

    更新 bestdomain.py

--- bestdomain.py
+++ bestdomain.py
@@ -20,7 +20,17 @@ def get_huawei_zone_id(client):
         return response.zones[0].id
     else:
         raise Exception("No zones found")
-
+def get_huawei_record_id(client):
+    # 根据实际情况调整查询条件获取域名的 zone_id
+    request = ListRecordSetsRequest(zone_id=zone_id)
+    response = client.list_record_sets(request)
+    # 假设这里第一个结果的 zone_id 就是要找的，实际情况可能需要更复杂的逻辑
+    if response.recordsets:
+        return response.recordsets[0].id
+    else:
+        raise Exception("No zones found")
+        
+        
 def delete_existing_dns_records(client, zone_id, subdomain, domain):
     record_name = domain if subdomain == '@' else f'{subdomain}.{domain}'
     while True:
@@ -69,6 +79,7 @@ if __name__ == "__main__":
     try:
         # 获取华为云域区 ID 和域名
         zone_id = get_huawei_zone_id(client)
+        recordset_id = get_huawei_record_id(client)
         
         for subdomain, url in subdomain_ip_mapping.items():
             # 获取 IP 列表
