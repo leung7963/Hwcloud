8653ac180e9b1c75e1131df21d51043c9daefd4b
leung7963 <104255180+leung7963@users.noreply.github.com>
2024-08-27 12:22:17 +0800

    更新 bestdomain.py

--- bestdomain.py
+++ bestdomain.py
@@ -13,8 +13,8 @@ def get_ip_list(url):
 
 def get_huawei_zone_id(client):
     # 根据实际情况调整查询条件获取域名的 zone_id
-    request = ShowRecordSetByZoneRequest()
-    response = client.show_record_set_by_zone(request)
+    request = ListPublicZonesRequest()
+    response = client.list_public_zones(request)
     # 假设这里第一个结果的 zone_id 就是要找的，实际情况可能需要更复杂的逻辑
     if response.zones:
         return response.zones[0].id
@@ -22,8 +22,8 @@ def get_huawei_zone_id(client):
         raise Exception("No zones found")
 def get_huawei_record_id(client):
     # 根据实际情况调整查询条件获取域名的 zone_id
-    request = ListRecordSetsRequest(zone_id=zone_id)
-    response = client.list_record_sets(request)
+    request = ShowRecordSetByZoneRequest(zone_id=zone_id)
+    response = client.show_record_set_by_zone(request)
     # 假设这里第一个结果的 zone_id 就是要找的，实际情况可能需要更复杂的逻辑
     if response.recordsets:
         return response.recordsets[0].id
