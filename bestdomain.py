2e0ca8fd98b98fc10444f62c586caf10bbfecf13
leung7963 <104255180+leung7963@users.noreply.github.com>
2024-09-15 07:56:50 +0800

    更新 bestdomain.py

--- bestdomain.py
+++ bestdomain.py
@@ -79,7 +79,7 @@ else:
                 body={
                     "name": domain_name + ".",
                     "type": "A",
-                    "ttl": 3600,
+                    "ttl": 1,
                     "records": [ip],
                     "weight": "1"
                 }
